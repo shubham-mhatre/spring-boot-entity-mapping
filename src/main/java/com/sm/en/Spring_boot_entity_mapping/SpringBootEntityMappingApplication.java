39c422ac84cbf136fb452dfbbe9f0bdc14da3ddc
shubham mhatre <shubhammahtre@bdo.in>
2024-06-26 11:22:29 +0530

    entity mapping empty initial commit

---
+++ src/main/java/com/sm/en/Spring_boot_entity_mapping/SpringBootEntityMappingApplication.java
@@ -0,0 +1,13 @@
+package com.sm.en.Spring_boot_entity_mapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEntityMappingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEntityMappingApplication.class, args);
+	}
+
+}
